6dac23490d422736526eec3bca17a508ea21a2e2
Barbara Schvuchov <schvuchov.k@gmail.com>
2024-06-27 21:32:36 -0300

    achei erro + navbar,start e about para 1024respons

--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -4,16 +4,18 @@ import "./style.css"
 const Start = () => {
     return (
         <section id="startSection" className='startSec'>
-            <div className='conteudoSec'>
-                <p className='startp'>Olá! Meu nome é Bárbara e esse é o meu portfolio</p>
-                <h1>Dev <span>Front End</span></h1>
-                <p className='startp'>Fique a vontade para explorar por aqui</p>
-                <button className='btn btnContato'>Entre em contato</button>    
+            <div className='divStartSec'>
+                <div className='conteudoSec'>
+                    <p className='startp'>Olá! Meu nome é Bárbara e esse é o meu portfolio</p>
+                    <h1>Dev <span>Front End</span></h1>
+                    <p className='startp'>Fique a vontade para explorar por aqui</p>
+                    <button className='btn btnContato'>Entre em contato</button>    
+                </div>
+                <div className='imgStart'>
+                    <img src="/Imagens/startImg1.jpg" alt="Imagem Start" />
+                </div>
             </div>
-            <div className='imgStart'>
-                <img src="/Imagens/startImg1.jpg" alt="Imagem Start" />
-
-            </div>
+            
         </section>
     );
 };
